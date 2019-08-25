3cc2672113be6c58077653cac8c9592a583998b3
Mike Wilson <mail4mikew@gmail.com>
2019-08-24 20:48:47 -0700

    try to get brightness to change correctly

--- lighting.cpp
+++ lighting.cpp
@@ -16,19 +16,16 @@
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
 
-void Lighting::setup(int brightness)
+void Lighting::setup(int bright)
 {
 	// It's important to set the color correction for your LED strip here,
 	// so that colors can be more accurately rendered through the 'temperature' profiles
 	FastLED.addLeds<CHIPSET, LEDL, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalSMD5050 );
 	FastLED.addLeds<CHIPSET, LEDR, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalSMD5050 );
-	FastLED.setBrightness( brightness );
-	FastLED.setTemperature( HighNoonSun );
+	brightness( bright );
+	FastLED.setTemperature( DirectSunlight );
 
-	for(int x = 0; x < NUM_LEDS;x++)
-	{
-		leds[x] = CRGB::White;
-	}
+	fill_solid(leds, NUM_LEDS, CRGB::White);
 }
 
 void Lighting::show()
@@ -40,6 +37,7 @@ void Lighting::brightness(int percent)
 {
 	int scaled = (percent * 255) + 50 / 100;	// scale 0-100 to 0-255
 	FastLED.setBrightness(scaled);
+	fill_solid(leds, NUM_LEDS, CRGB::Tomato);
 	FastLED.show();
 }
 
